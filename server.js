63487f690c96e7ac04a232cd5c0bc497a5502419
Jhun Wulf Sabala <sabala.jw@nec.com>
2025-03-03 10:30:26 +0800

    all

--- server.js
+++ server.js
@@ -8,13 +8,10 @@ const db = require('./config/db');
 dotenv.config();
 
 const app = express();
-app.use(cors({ origin: ['https://eduflick-client-one.vercel.app/'] })); // Allows requests from any origin
+app.use(cors({ origin: ['*'] })); // Allows requests from any origin
 app.use(bodyParser.json());
 
 app.use('/api/auth', authRoutes);
-app.get('/api', (req, res) => {
-    res.json({ message: 'Online' });
-});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
