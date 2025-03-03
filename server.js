9558cf61292ea56c24a566497ea87bd80bc1f48a
Jhun Wulf Sabala <sabala.jw@nec.com>
2025-03-03 10:26:13 +0800

    cors

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ app.use(cors({ origin: ['https://eduflick-client-one.vercel.app/'] })); // Allow
 app.use(bodyParser.json());
 
 app.use('/api/auth', authRoutes);
+app.get('/api', (req, res) => {
+    res.json({ message: 'Online' });
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
