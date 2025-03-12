dc9de1e764952d2ec5f8028a1bfdbb028439e13d
Jhun Wulf Sabala <sabala.jw@nec.com>
2025-03-13 00:58:55 +0800

    corss

--- config/db.js
+++ config/db.js
@@ -1,20 +1,23 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const db = mysql.createConnection({
+const poolDB = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,  // ✅ Maintain 10 connections
+  queueLimit: 0
 });
 
-db.connect(err => {
-  if (err) {
-    console.error('Database connection failed:', err);
-  } else {
-    console.log('Connected to MySQL');
-  }
-});
+// db.connect(err => {
+//   if (err) {
+//     console.error('Database connection failed:', err);
+//   } else {
+//     console.log('Connected to MySQL');
+//   }
+// });
 
-module.exports = db;
+module.exports = poolDB.promise();;
